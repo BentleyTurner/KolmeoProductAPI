07a15e4738a660ff7c2ee26bfba1d36e2c8461c5
BentleyTurner <bentleyturner@live.com>
2023-04-01 22:29:34 +1100

    Adding DB context to main

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+namespace KolmeoProductAPI.Models
+{
+    public class Product
+    {
+        private int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Price { get; set; }
+    }
+}
